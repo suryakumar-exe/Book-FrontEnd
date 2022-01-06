f54708d7e18ae20bb70941aadd558ed535a3b961
suryakumar-exe <suryakumar2808@gmail.com>
2022-01-06 16:52:39 +0530

    fr

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -8,22 +8,22 @@ export class AppService {
   constructor(private httpClient :HttpClient) { }
 
   getContacts(){
-    return this.httpClient.get('https://localhost:44394/api/Books/FetchAllBooks');
+    return this.httpClient.get('https://bookslibrary.azurewebsites.net/api/Books/FetchAllBooks');
   }
  
   createbook(newData: { id: number; title: string; author: string; description: string; }){
     const httpHeaders = new HttpHeaders();
     httpHeaders.append('content-type','application/json');
-    return this.httpClient.post('https://localhost:44394/api/Books/CreateBook',newData,{headers:httpHeaders});
+    return this.httpClient.post('https://bookslibrary.azurewebsites.net/api/Books/CreateBook',newData,{headers:httpHeaders});
   }
 
   deletebook(bookid: number){
-    const del = 'https://localhost:44394/api/Books/Delete/'+bookid;
+    const del = 'https://bookslibrary.azurewebsites.net/api/Books/Delete/'+bookid;
     return this.httpClient.delete(del);
   }
 
   updatebook(bookid: number, updateData: { id: number; title: string; author: string; description: string; }){
-      const upd= 'https://localhost:44394/api/Books/UpdateBook/'+bookid;
+      const upd= 'https://bookslibrary.azurewebsites.net/api/Books/UpdateBook/'+bookid;
       return this.httpClient.put(upd,updateData);
   }
 }
